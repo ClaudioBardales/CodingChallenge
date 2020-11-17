998663daf49215fab6ad7063d166f268025b356e
root <root@Claudio-PC.localdomain>
2020-11-17 00:02:52 -0800

    WIP Project Done JSON Render

---
+++ app.js
@@ -0,0 +1,39 @@
+// FETCH ITEMS
+
+async function getItems() {
+  let url = 'mock.json';
+  try {
+    let res = await fetch(url);
+    return await res.json();
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+console.log(getItems());
+
+async function renderItems() {
+  let items = await getItems();
+  let html = '';
+  items.forEach(item => {
+    let htmlSegment = ` <div class="card">
+                        <img src="${item.image_url}" alt="placeholder">
+                        <div class="descriptions">
+                        <p>${item.product_name}</p>
+                        <button>${item.price}</button>
+                        </div>
+                        </div>
+                        `;
+    html += htmlSegment;
+  });
+
+    let container = document.querySelector('.card-section');
+    container.innerHTML = html;
+
+    console.log(container);
+}
+
+renderItems();
+
+
+
